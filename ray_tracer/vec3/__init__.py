de697767f104705b4b15327a08088d8873f57cb4
shubham <shubham@Dell-G5-5587>
2022-05-04 00:55:31 +0100

    Update __init__.py

--- ray_tracer/vec3/__init__.py
+++ ray_tracer/vec3/__init__.py
@@ -18,7 +18,7 @@ class VectorTypeError(TypeError):
 # Base Vector class
 
 @dataclass
-class Vector:
+class Vector3D:
     v1: Union[int, float] = 0.0
     v2: Union[int, float] = 0.0
     v3: Union[int, float] = 0.0
@@ -35,8 +35,8 @@ class Vector:
     def z(self) -> Union[int, float]:
         return self.v3
 
-    def __neg__(self) -> Vector:
-        return Vector(-self.v1, -self.v2, -self.v3)
+    def __neg__(self) -> Vector3D:
+        return Vector3D(-self.v1, -self.v2, -self.v3)
 
     def __getitem__(self, index: int) -> Union[float, int]:
         if index == 0:
@@ -51,42 +51,42 @@ class Vector:
     def __len__(self) -> int:
         return 3
 
-    def __add__(self, other: Vector) -> Vector:
-        assert isinstance(other, Vector)
-        return Vector(self.v1 + other.v1, self.v2 + other.v2, self.v3 + other.v3)
+    def __add__(self, other: Vector3D) -> Vector3D:
+        assert isinstance(other, Vector3D)
+        return Vector3D(self.v1 + other.v1, self.v2 + other.v2, self.v3 + other.v3)
 
-    def __sub__(self, other: Vector) -> Vector:
-        assert isinstance(other, Vector)
-        return Vector(self.v1 - other.v1, self.v2 - other.v2, self.v3 - other.v3)
+    def __sub__(self, other: Vector3D) -> Vector3D:
+        assert isinstance(other, Vector3D)
+        return Vector3D(self.v1 - other.v1, self.v2 - other.v2, self.v3 - other.v3)
 
-    def __mul__(self, other: Union[Vector, int, float]) -> Vector:
-        if isinstance(other, Vector):
-            return Vector(self.v1 * other.v1, self.v2 * other.v2, self.v3 * other.v3)
+    def __mul__(self, other: Union[Vector3D, int, float]) -> Vector3D:
+        if isinstance(other, Vector3D):
+            return Vector3D(self.v1 * other.v1, self.v2 * other.v2, self.v3 * other.v3)
         elif isinstance(other, int) or isinstance(other, float):
-            return Vector(self.v1 * other, self.v2 * other, self.v3 * other)
+            return Vector3D(self.v1 * other, self.v2 * other, self.v3 * other)
         else:
             raise VectorTypeError("Input must be Vector, int or float")
 
-    def __rmul__(self, other: Union[Vector, float, int]) -> Vector:
+    def __rmul__(self, other: Union[Vector3D, float, int]) -> Vector3D:
         return self * other
 
-    def __truediv__(self, other: Union[Vector, float, int]) -> Vector:
+    def __truediv__(self, other: Union[Vector3D, float, int]) -> Vector3D:
         return (1.0 / other) * self
 
-    def __iadd__(self, other: Vector) -> Vector:
-        assert isinstance(other, Vector)
+    def __iadd__(self, other: Vector3D) -> Vector3D:
+        assert isinstance(other, Vector3D)
         self.v1 += other.v1
         self.v2 += other.v2
         self.v3 += other.v3
         return self
 
-    def __imul__(self, other: Union[float, int]) -> Vector:
+    def __imul__(self, other: Union[float, int]) -> Vector3D:
         self.v1 *= other
         self.v2 *= other
         self.v3 *= other
         return self
 
-    def __itruediv__(self, other: Union[float, int]) -> Vector:
+    def __itruediv__(self, other: Union[float, int]) -> Vector3D:
         return self.__imul__(1.0 / other)
 
     def print_vector(self) -> None:
@@ -101,27 +101,27 @@ class Vector:
 
 # Type aliases for Vector
 
-class Point3D(Vector):
+class Point3D(Vector3D):
     def __init__(self, x=0.0, y=0.0, z=0.0):
         super(Point3D, self).__init__(x, y, z)
 
 
-class Colour(Vector):
+class Colour(Vector3D):
     def __init__(self, r=0.0, g=0.0, b=0.0):
         super(Colour, self).__init__(r, g, b)
 
 
 # Utility functions
 
-def dot(u: Vector, v: Vector) -> Union[int, float]:
+def dot(u: Vector3D, v: Vector3D) -> Union[int, float]:
     return u.x * v.x + u.y * v.y + u.z * v.z
 
 
-def cross(u: Vector, v: Vector) -> Vector:
-    return Vector(u[1] * v[2] - u[2] * v[1],
-                  u[2] * v[0] - u[0] * v[2],
-                  u[0] * v[1] - u[1] * v[0])
+def cross(u: Vector3D, v: Vector3D) -> Vector3D:
+    return Vector3D(u[1] * v[2] - u[2] * v[1],
+                    u[2] * v[0] - u[0] * v[2],
+                    u[0] * v[1] - u[1] * v[0])
 
 
-def to_unit_vector(v: Vector) -> Vector:
+def to_unit_vector(v: Vector3D) -> Vector3D:
     return v / v.l2_norm()
